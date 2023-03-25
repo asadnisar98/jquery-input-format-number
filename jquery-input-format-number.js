3a761181d3c503aa29d2560aa50ce763337673e4
flopmart <34133163+asadnisar98@users.noreply.github.com>
2023-03-25 23:25:32 +0500

    final publish

---
+++ jquery-input-format-number.js
@@ -0,0 +1,36 @@
+(function($) {
+    $.fn.numberFormatter = function(options) {
+      const settings = $.extend({
+        thousandSeparator: ',',
+        decimalSeparator: '.'
+      }, options);
+  
+      return this.each(function() {
+        const $input = $(this);
+        $input.on('input', function() {
+          let value = $input.val().replace(/\D/g, '');
+          const decimalIndex = value.indexOf(settings.decimalSeparator);
+          const integerPart = decimalIndex > -1 ? value.slice(0, decimalIndex) : value;
+          const integerPartWithSeparator = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, settings.thousandSeparator);
+          const formattedValue = decimalIndex > -1 ? integerPartWithSeparator + settings.decimalSeparator + value.slice(decimalIndex + 1) : integerPartWithSeparator;
+          $input.val(formattedValue);
+        });
+  
+        $input.on('blur', function() {
+          let value = $input.val();
+          value = value.replace(new RegExp('\\' + settings.thousandSeparator, 'g'), '');
+          value = value.replace(settings.decimalSeparator, '.');
+  
+          if (!isNaN(value)) {
+            const formattedValue = parseFloat(value).toLocaleString('en-US', {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2
+            });
+  
+            $input.val(formattedValue);
+          }
+        });
+      });
+    };
+  }(jQuery));
+  
